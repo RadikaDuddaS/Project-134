a42ea1c6abc186cbf556d3b806a5b570624bf696
RadikaDuddaS <102700155+RadikaDuddaS@users.noreply.github.com>
2022-06-09 18:35:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+sound = "";
+Status = "";
+
+function preload(){
+    sound = createVideo('my_best_friend.mp3');
+}
+
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+}
+
+function draw(){
+    image(video, 0,0,480,380);
+
+    for (let index = 0; index < objects.length; index++) {
+
+        if(objects[index].label  == "person")
+        {
+            document.getElementById("baby_status").innerHTML = "Status : Baby Detected";
+       
+            sound.stop()
+        }
+
+        else(objects[index].label != "person")
+        {
+            document.getElementById("baby_status").innerHTML = "Status : Baby Not Detected";
+        
+            sound.play()
+        }
+
+        }
+
+        if(objects.length < 0)
+        {
+            document.getElementById("baby_status").innerHTML = "Status : Baby Not Detected";
+        
+            sound.play()
+        }
+
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("Status").innerHTML = "Status : Baby Detected";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
